2678ac32f69403ce90ac1dfb269cb872df873bb3
swethaep99 <85289802+swethaep99@users.noreply.github.com>
2025-06-19 23:47:32 +0530

    Added category model and it's frontend part

--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField( max_length=50,unique=True)
+    slug = models.SlugField(unique=True,blank=True)
+    image = models.ImageField(upload_to='category_images/')
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
--- products/views.py
+++ products/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
+def products(request):
+    return render(request,'products.html')
+
 def shop(request):
-    return render(request,'shop.html')
+    categories = Categories.objects.all()
+    return render(request,'shop.html',{'categories' : categories})
