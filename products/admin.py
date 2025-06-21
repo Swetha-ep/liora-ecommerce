2cfee6f6e5b05b8905c66aa986a26d2771ec086e
swethaep99 <85289802+swethaep99@users.noreply.github.com>
2025-06-21 14:52:32 +0530

    added models for products, color, sizes, inventory

--- products/admin.py
+++ products/admin.py
@@ -3,3 +3,7 @@ from .models import *
 # Register your models here.
 
 admin.site.register(Categories)
+admin.site.register(Product)
+admin.site.register(Color)
+admin.site.register(Size)
+admin.site.register(Inventory)
--- products/models.py
+++ products/models.py
@@ -15,3 +15,49 @@ class Categories(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150,unique=True)
+    slug = models.SlugField(unique=True,blank=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+    
+
+class Color(models.Model):
+    name = models.CharField(max_length=50)
+    hex_code = models.CharField(max_length=7, blank=True, help_text="e.g. #FF5733")
+
+    def __str__(self):
+        return self.name
+    
+
+class Size(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+    
+
+class Inventory(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    stock = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='product_images',blank=True,null=True)
+
+    class Meta:
+        unique_together = ('product','color','size')
+    
+    def __str__(self):
+        return f"{self.product.name} -{self.color.name} - {self.size.name}"
